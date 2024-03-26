fe05d35b794f5889d2595d4c457f9ba734ea44be
Admin <Admin@THANH-TUNG>
2024-03-26 09:03:46 +0700

    Add project files.

---
+++ ThucHanh26-03/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace ThucHanh26_03
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class HelloWorld : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            MessageBox.Show("Hello world");
+            return Result.Succeeded;
+        }
+    }
+}
